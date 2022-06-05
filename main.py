11d3415dba5b3d14b85dcab829903641670df369
Jake <91381193+MaundoJako@users.noreply.github.com>
2022-06-05 08:06:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Data Types
+
+#string
+
+print("Hello"[4])
+
+#This is called subscripting - pulling out a character
+
+#Integer
+
+print(123_343_542+4355)
+
+#underscore represents commas in big numbers, computer ignores it
+
+#Float
+#Has a decimal point
+
+3.13455
+
+#Boolean
+
+True
+False
